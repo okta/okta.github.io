627b0cd33e6b1dd57d986a67e45189a0039cc107
Mysti Berry <mysti.berry@okta.com>
2017-07-04 23:13:32 -0700

    beef up OIDC and auth concept topics
    commented out flapper
    
    future-proof reference to okta admin
    
    final comments
    
    more final comments and un-commenting out test
    
    comments and test fix
    
    fixed hyphen

--- tests/selenium/spec/table-of-contents-spec.js
+++ tests/selenium/spec/table-of-contents-spec.js
@@ -15,10 +15,10 @@ describe('table of contents navigation spec', () => {
   it('has basic table of contents in the documentation page', () => {
     expect(tocPage.level1ItemContains('Authentication')).toBe(true);
 
-    const expectedLevel2Items = ['Introduction', 'Building apps supporting Single Sign-On',
-        'Building custom login experience for your application',
-        'Sign-in Widget', 'Auth SDK – a lightweight Javascript-based SDK',
-        'Authentication APIs – REST APIs for any client', 'Social Authentication'];
+    const expectedLevel2Items = ['Authentication with Okta', 'Building Apps That Support Single Sign On',
+        'Building a Custom Login Experience for Your Application',
+        'Sign-In Widget', 'Auth SDK',
+        'Authentication API', 'Social Login'];
     expect(tocPage.level2ItemsContain(expectedLevel2Items)).toBe(true);
   });
 
