972de76a61bbdbb285569e293eafe5109a277080
hamza <hamza@flickerbox.com>
2017-08-15 16:09:39 -0700

    update browser sizes

--- tests/selenium/framework/page-objects/BasePage.js
+++ tests/selenium/framework/page-objects/BasePage.js
@@ -5,93 +5,92 @@ const EC = protractor.ExpectedConditions;
 const baseUrl = 'http://localhost:4000';
 
 class BasePage {
-  constructor(relativeURL) {
-    this.$pageLoad = null;
-    if(relativeURL) {
-      this.url = baseUrl + relativeURL;
-    } else {
-      this.url = baseUrl;
-    }
-  }
+	constructor(relativeURL) {
+		this.$pageLoad = null;
+		if(relativeURL) {
+			this.url = baseUrl + relativeURL;
+		} else {
+			this.url = baseUrl;
+		}
+	}
 
-  setPageLoad(element) {
-    this.$pageLoad = element;
-  }
+	setPageLoad(element) {
+		this.$pageLoad = element;
+	}
 
-  load() {
-    browser.ignoreSynchronization = true;
-    browser.get(this.url);
-    return this.waitForPageLoad();
-  }
+	load() {
+		browser.ignoreSynchronization = true;
+		browser.get(this.url);
+		return this.waitForPageLoad();
+	}
 
-  waitForPageLoad() {
-    return util.wait(this.$pageLoad);
-  }
+	waitForPageLoad() {
+		return util.wait(this.$pageLoad);
+	}
 
-  setWindowSize(width, height) {
-    browser.driver.manage().window().setSize(width, height);
-  }
+	setWindowSize(width, height) {
+		browser.driver.manage().window().setSize(width, height);
+	}
 
-  waitUntilOnScreen(elementFinder) {
-    browser.wait(util.isOnScreen(elementFinder));
-  }
+	waitUntilOnScreen(elementFinder) {
+		browser.wait(util.isOnScreen(elementFinder));
+	}
 
-  waitUntilOffScreen(elementFinder) {
-    browser.wait(EC.not(util.isOnScreen(elementFinder)));
-  }
+	waitUntilOffScreen(elementFinder) {
+		browser.wait(EC.not(util.isOnScreen(elementFinder)));
+	}
 
-  elementsContainText(elements, expectedTextArray) {
-    if (!Array.isArray(expectedTextArray)) {
-      expectedTextArray = [expectedTextArray];
-    }
-    return elements.filter((element, index) => {
-      return element.getText().then((text) => {
-        return expectedTextArray.indexOf(text) > -1;
-      });
-    }).then((elementList) => {
-      return elementList.length == expectedTextArray.length;
-    })
-  }
-  
-  urlContains(str) {
-    return EC.urlContains(str)();
-  }
+	elementsContainText(elements, expectedTextArray) {
+		if (!Array.isArray(expectedTextArray)) {
+			expectedTextArray = [expectedTextArray];
+		}
+		return elements.filter((element, index) => {
+			return element.getText().then((text) => {
+				return expectedTextArray.indexOf(text) > -1;
+			});
+		}).then((elementList) => {
+			return elementList.length == expectedTextArray.length;
+		})
+	}
 
-  getCurrentURL() {
-    return browser.getCurrentUrl().then(url => url.replace(baseUrl, ''));
-  }
-  
-  // These are values used in css for managing different browser sizes -
-  // max-width: 1599px -> xxLarge
-  // max-width: 1399px -> xLarge
-  // max-width: 1199px -> large
-  // max-width: 1023px -> medium
-  // max-width: 899px -> mediumSmall
-  // max-width: 767px -> small
-  // max-width: 479px -> xSmall
-  // max-width: 319px -> xxSmall
-  // setSize() calls fail on headless chrome due to chromedriver issue
-  resizeMedium() {
-    if (!process.env.CHROME_HEADLESS) {
-      browser.driver.manage().window().setSize(1060, 640);
-    }
-  }
+	urlContains(str) {
+		return EC.urlContains(str)();
+	}
 
-  resizeXsmall() {
-    if (!process.env.CHROME_HEADLESS) {
-      browser.driver.manage().window().setSize(480, 640);
-    }
-  }
+	getCurrentURL() {
+		return browser.getCurrentUrl().then(url => url.replace(baseUrl, ''));
+	}
 
-  resizeXLarge() {
-    if (!process.env.CHROME_HEADLESS) {
-      browser.driver.manage().window().setSize(1560, 840);
-    }
-  }
+	// These are values used in css for managing different browser sizes -
+	// 'xx-small': 480px,
+	// 'x-small': 600px,
+	// 'small': 768px,
+	// 'medium': 1024px,
+	// 'large': 1200px,
+	// 'x-large': 1400px,
+	// 'xx-large': 1600px
+	// setSize() calls fail on headless chrome due to chromedriver issue
+	resizeMedium() {
+		if (!process.env.CHROME_HEADLESS) {
+			browser.driver.manage().window().setSize(1024, 640);
+		}
+	}
 
-  hasElements(elements) {
-    return elements.then(element => element.length > 0);
-  }
+	resizeXXsmall() {
+		if (!process.env.CHROME_HEADLESS) {
+			browser.driver.manage().window().setSize(480, 640);
+		}
+	}
+
+	resizeXLarge() {
+		if (!process.env.CHROME_HEADLESS) {
+			browser.driver.manage().window().setSize(1400, 840);
+		}
+	}
+
+	hasElements(elements) {
+		return elements.then(element => element.length > 0);
+	}
 }
 
 module.exports = BasePage;
