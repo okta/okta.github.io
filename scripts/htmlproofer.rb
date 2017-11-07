06d55d1267620bb8303863f5c46f5e3596d503ad
jmelberg <jordan.melberg@okta.com>
2017-11-06 22:40:04 -0800

    Updated external htmlproofer to only look for 4xx errors

--- scripts/htmlproofer.rb
+++ scripts/htmlproofer.rb
@@ -7,6 +7,7 @@ options = {
     :allow_hash_href => true,
     :empty_alt_ignore => true,
     :log_level => :debug,
+    :only_4xx => true,
     # # cache external results for 1 day in ~/.htmlproofer/cache
     # :cache => { :timeframe => '1d', :storage_dir => "#{Dir.home}/.htmlproofer/cache"},
     # 8 threads, any more doesn't seem to make a difference
@@ -19,15 +20,10 @@ options = {
         /python_api_sdk/,
         /javadoc/,
         /csharp_api_sdk/,
-
-        # files no longer manged by gh-pages
-        /404.html/,
-        /error.html/,
-        /integrate_with_okta\/index.html/
     ],
     :url_ignore => [
-        /localhost/, # tons of 'localhosts' for examples
         /linkedin.com/, # linked in doesn't play nice with site crawlers
-        /stormpath.com/] # 😢
+        /stormpath.com/ # 😢
+    ]
 }
 HTMLProofer.check_directory('dist', options).run
