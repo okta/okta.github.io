8e8fc9d5ef43c78aff4ccd516a2c2cf7a974d900
jmelberg <jordan.melberg@okta.com>
2018-01-22 15:46:48 -0800

    Updated test structure for Travis

--- scripts/htmlproofer.rb
+++ scripts/htmlproofer.rb
@@ -2,13 +2,21 @@
 
 require 'html-proofer'
 
+# Ruby hack to convert params to boolean
+class String
+    def to_bool
+      return true   if self == true   || self =~ (/(true)$/i)
+      return false  if self == false  || self.blank? || self =~ (/(false)$/i)
+      raise ArgumentError.new("invalid value for Boolean: \"#{self}\"")
+    end
+end
+
 options = {
-    :assume_extension => true,
+    :assume_extension => ARGV[0].to_bool,
     :allow_hash_href => true,
     :empty_alt_ignore => true,
     :log_level => :error,
     :only_4xx => true,
-    :cache => { :timeframe => '1d' },
     # 8 threads, any more doesn't seem to make a difference
     :parallel => { :in_processes => 8},
     :file_ignore => [
