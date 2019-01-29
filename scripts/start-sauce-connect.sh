58dd6b9b9017fa5886fb24e6b7bac3153f12bfc5
Wayne Earl <wayne.earl@okta.com>
2019-01-29 14:23:01 -0700

    add job number as tunnel ID

--- scripts/start-sauce-connect.sh
+++ scripts/start-sauce-connect.sh
@@ -3,6 +3,7 @@ set -e
 
 if [[ $TRAVIS_EVENT_TYPE == 'push' ]]
 then
+  env | sort
   tar -xvzf sc-4.5.3-linux.tar.gz &&
-  ./sc-4.5.3-linux/bin/sc -vv -l sc.log &
+  ./sc-4.5.3-linux/bin/sc -i $TRAVIS_JOB_NUMBER -l sc.log &
 fi
